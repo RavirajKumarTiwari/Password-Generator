1777ba892da147830891718c2cd7c22371dfec5a
Raviraj Kumar <ravirajk545@gmail.com>
2023-03-27 21:57:52 +0530

    adding assets and changes to the index.html and script.js files

---
+++ script.js
@@ -0,0 +1,4 @@
+const inputSlider = document.querySelector("[data-lenghtSlider]");
+const lenghtDsiplay = document.querySelector("[data-lenghtNumber]");
+
+const passwordDisplay = document.querySelector("[data-pass]")
